0fa978c3e987e8d91c00cc3e79b33af6b45b2435
Tom Rothamel <tom@rothamel.us>
2014-11-25 00:13:28 -0500

    SDL2 pyjnius.

--- recipes/pygame_sdl2/recipe.sh
+++ recipes/pygame_sdl2/recipe.sh
@@ -32,6 +32,7 @@ function build_pygame_sdl2() {
 	push_arm
 
     CFLAGS="$CFLAGS -DANDROID -I$JNI_PATH/sdl2/include -I$JNI_PATH/sdl2_image -I$JNI_PATH/sdl2_gfx -I$JNI_PATH/sdl2_ttf -I$JNI_PATH/png -I$JNI_PATH/freetype/include"
+
 	export CFLAGS="$CFLAGS"
 	export LDFLAGS="$LDFLAGS -L$LIBS_PATH -L$SRC_PATH/obj/local/$ARCH/ -lm -lz"
 	export LDSHARED="$LIBLINK"
--- recipes/pyjnius/recipe.sh
+++ recipes/pyjnius/recipe.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-VERSION_pyjnius=${VERSION_pyjnius:-master}
-URL_pyjnius=https://github.com/kivy/pyjnius/archive/$VERSION_pyjnius.zip
+VERSION_pyjnius= # ${VERSION_pyjnius:-master}
+URL_pyjnius= # https://github.com/kivy/pyjnius/archive/$VERSION_pyjnius.zip
 DEPS_pyjnius=(python sdl)
 MD5_pyjnius=
-BUILD_pyjnius=$BUILD_PATH/pyjnius/$(get_directory $URL_pyjnius)
+BUILD_pyjnius=$BUILD_PATH/pyjnius # /$(get_directory $URL_pyjnius)
 RECIPE_pyjnius=$RECIPES_PATH/pyjnius
 
+CHECKOUT="$BUILD_pyjnius/pyjnius/src"
+
 function prebuild_pyjnius() {
-	true
+	if [ ! -e $CHECKOUT ] ; then
+        try git clone https://github.com/renpy/pyjnius.git $CHECKOUT
+    fi
 }
 
 function shouldbuild_pyjnius() {
-	if [ -d "$SITEPACKAGES_PATH/jnius" ]; then
-		DO_BUILD=0
-	fi
+    true;
+
+	# if [ -d "$SITEPACKAGES_PATH/jnius" ]; then
+		# 	DO_BUILD=0
+	# fi
 }
 
 function build_pyjnius() {
-	cd $BUILD_pyjnius
+	try cd $CHECKOUT
 
 	push_arm
 
 	export LDFLAGS="$LDFLAGS -L$LIBS_PATH"
 	export LDSHARED="$LIBLINK"
+    export PYJNIUS_SDL_VERSION=2
 
 	# fake try to be able to cythonize generated files
 	$HOSTPYTHON setup.py build_ext
