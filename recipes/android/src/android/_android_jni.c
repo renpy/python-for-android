5ea5f1d6ccc2736e6f4fc33c8a0b69aefa3a0092
Tom Rothamel <tom@rothamel.us>
2014-11-26 20:32:28 -0500

    Remove obsolete functions from the android module.
    Many of these functions are available through SDL2, and should be
    moved into pygame_sdl2.

--- recipes/android/src/android/_android_jni.c
+++ recipes/android/src/android/_android_jni.c
@@ -29,89 +29,6 @@ void android_vibrate(double seconds) {
         (jdouble) seconds);
 }
 
-void android_accelerometer_enable(int enable) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "accelerometerEnable", "(Z)V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(
-        env, cls, mid,
-        (jboolean) enable);
-}
-
-void android_wifi_scanner_enable(void){
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "enableWifiScanner", "()V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(env, cls, mid);
-}
-
-
-char * android_wifi_scan() {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-    jobject jreading;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "scanWifi", "()Ljava/lang/String;");
-        aassert(mid);
-    }
-
-    PUSH_FRAME;
-    jreading = (*env)->CallStaticObjectMethod(env, cls, mid);
-    const char * reading = (*env)->GetStringUTFChars(env, jreading, 0);
-    POP_FRAME;
-
-    return reading;
-}
-
-void android_accelerometer_reading(float *values) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-    jobject jvalues;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "accelerometerReading", "()[F");
-        aassert(mid);
-    }
-
-    PUSH_FRAME;
-
-    jvalues = (*env)->CallStaticObjectMethod(env, cls, mid);
-    (*env)->GetFloatArrayRegion(env, jvalues, 0, 3, values);
-
-    POP_FRAME;
-}
-
 int android_get_dpi(void) {
     static JNIEnv *env = NULL;
     static jclass *cls = NULL;
@@ -127,40 +44,6 @@ int android_get_dpi(void) {
     }
 
     return (*env)->CallStaticIntMethod(env, cls, mid);
-}
-
-void android_show_keyboard(int input_type) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "showKeyboard", "(I)V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(env, cls, mid, (jint) input_type);
-}
-
-void android_hide_keyboard(void) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/Hardware");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "hideKeyboard", "()V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(env, cls, mid);
 }
 
 char* BUILD_MANUFACTURER = NULL;
@@ -201,57 +84,6 @@ void android_get_buildinfo() {
     }
 }
 
-void android_activate_input(void) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/SDLSurfaceView");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "activateInput", "()V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(env, cls, mid);
-}
-
-int android_checkstop(void) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/SDLSurfaceView");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "checkStop", "()I");
-        aassert(mid);
-    }
-
-    return (*env)->CallStaticIntMethod(env, cls, mid);
-}
-
-void android_ackstop(void) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/SDLSurfaceView");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "ackStop", "()I");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticIntMethod(env, cls, mid);
-}
-
 void android_action_send(char *mimeType, char *filename, char *subject, char *text, char *chooser_title) {
     static JNIEnv *env = NULL;
     static jclass *cls = NULL;
@@ -288,69 +120,20 @@ void android_action_send(char *mimeType, char *filename, char *subject, char *te
 }
 
 void android_open_url(char *url) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
 
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/SDLSurfaceView");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "openUrl", "(Ljava/lang/String;)V");
-        aassert(mid);
-    }
+	JNIEnv* env = (JNIEnv*) SDL_AndroidGetJNIEnv();
+	jobject activity = (jobject) SDL_AndroidGetActivity();
+	jclass clazz = (*env)->GetObjectClass(env, activity);
+	jmethodID method_id = (*env)->GetMethodID(env, clazz, "openUrl", "(Ljava/lang/String;)V");
 
-    PUSH_FRAME;
+	PUSH_FRAME;
 
-    (*env)->CallStaticVoidMethod(
-        env, cls, mid,
-        (*env)->NewStringUTF(env, url)
-        );
+	(*env)->CallVoidMethod(env, activity, method_id, (*env)->NewStringUTF(env, url));
 
     POP_FRAME;
+
+	(*env)->DeleteLocalRef(env, activity);
+	(*env)->DeleteLocalRef(env, clazz);
+
 }
 
-void android_start_service(char *title, char *description, char *arg) {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        aassert(env);
-        cls = (*env)->FindClass(env, "org/renpy/android/PythonActivity");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "start_service",
-                                       "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
-        aassert(mid);
-    }
-
-    jstring j_title = NULL;
-    jstring j_description = NULL;
-    jstring j_arg = NULL;
-    if ( title != 0 )
-            j_title = (*env)->NewStringUTF(env, title);
-    if ( description != 0 )
-            j_description = (*env)->NewStringUTF(env, description);
-    if ( arg != 0 )
-            j_arg = (*env)->NewStringUTF(env, arg);
-
-    (*env)->CallStaticVoidMethod(env, cls, mid, j_title, j_description, j_arg);
-}
-
-void android_stop_service() {
-    static JNIEnv *env = NULL;
-    static jclass *cls = NULL;
-    static jmethodID mid = NULL;
-
-    if (env == NULL) {
-        env = (JNIEnv *) SDL_AndroidGetJNIEnv();
-        cls = (*env)->FindClass(env, "org/renpy/android/PythonActivity");
-        aassert(cls);
-        mid = (*env)->GetStaticMethodID(env, cls, "stop_service", "()V");
-        aassert(mid);
-    }
-
-    (*env)->CallStaticVoidMethod(env, cls, mid);
-}
